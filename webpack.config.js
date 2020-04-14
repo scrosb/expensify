29a6035f0f7220a52cd3d1a85eea43b6164daba1
scrosb <scrosb@gmail.com>
2020-04-14 12:51:37 -0500

    Setup Production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,92 @@
 
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-console.log();
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path:path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules:[{
-      loader: 'babel-loader',
-      //checks if the file loaded ends in js
-      test: /\.js$/,
-      //exclude node_modules folder
-      exclude: /node_modules/
+module.exports = (env) => {
+  const isProduction = env ==='production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
 
-    }, {
-      //anyfile that ends with .scss and css, the question mark makes it optional
-      test: /\.s?css$/,
-      //use provides us an array of loaders
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
-  },
-  devtool:'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    // historyAPIfallback-WE will be handling routing for our client side code
-    historyApiFallback:true
-  }
-};
+  return {
+    entry: './src/app.js',
+    output: {
+      path:path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules:[{
+        loader: 'babel-loader',
+        //checks if the file loaded ends in js
+        test: /\.js$/,
+        //exclude node_modules folder
+        exclude: /node_modules/
+  
+      }, {
+        //anyfile that ends with .scss and css, the question mark makes it optional
+        test: /\.s?css$/,
+        //use provides us an array of loaders
+        use: CSSExtract.extract({
+          use: [
+            {
+              loader:'css-loader',
+              options:{
+                sourceMap:true
+              }
+            },
+            {
+              loader: 'sass-loader', 
+              options:{
+                sourceMap:true
+              }
+            }
+          ]
+        })
+      }]
+    },
+    plugins: [
+      CSSExtract
+    ],
+    devtool:isProduction ? 'source-map': 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      // historyAPIfallback-WE will be handling routing for our client side code
+      historyApiFallback:true
+    }
+  };
+}
 
-//loader - run through babel
+// module.exports = {
+//   entry: './src/app.js',
+//   output: {
+//     path:path.join(__dirname, 'public'),
+//     filename: 'bundle.js'
+//   },
+//   module: {
+//     rules:[{
+//       loader: 'babel-loader',
+//       //checks if the file loaded ends in js
+//       test: /\.js$/,
+//       //exclude node_modules folder
+//       exclude: /node_modules/
+
+//     }, {
+//       //anyfile that ends with .scss and css, the question mark makes it optional
+//       test: /\.s?css$/,
+//       //use provides us an array of loaders
+//       use: [
+//         'style-loader',
+//         'css-loader',
+//         'sass-loader'
+//       ]
+//     }]
+//   },
+//   devtool:'cheap-module-eval-source-map',
+//   devServer: {
+//     contentBase: path.join(__dirname, 'public'),
+//     // historyAPIfallback-WE will be handling routing for our client side code
+//     historyApiFallback:true
+//   }
+// };
+
+// //loader - run through babel
 
