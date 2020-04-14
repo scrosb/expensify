a8c30d3e8b41878156e93fc3f3b7d581eb0ccc05
scrosb <scrosb@gmail.com>
2020-04-14 15:51:31 -0500

    changed server.js

--- server/server.js
+++ server/server.js
@@ -3,16 +3,11 @@ const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname,'..', 'public');
 
-
-
 app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
 
-app.listen(PORT => {
-  console.log(`Server started on port 3000`)
-});
+app.listen((process.env.PORT));
