bed845a1e9fccc6dc6493e8494b11673615e151c
scrosb <scrosb@gmail.com>
2020-04-14 11:08:35 -0500

    Remove Dummy Expense Data

--- src/app.js
+++ src/app.js
@@ -3,9 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //Provider will allow us to provide the store to all our components that make up our application
 import {Provider} from 'react-redux';
-
-
-
 import AppRouter from './routers/AppRouter'
 //import redux store
 import configureStore from './store/configureStore';
@@ -15,10 +12,6 @@ import 'react-dates/lib/css/_datepicker.css';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-import {addExpense, removeExpense, editExpense} from './actions/expenses';
-import {setTextFilter} from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
-
 const store = configureStore();
 
 // store.subscribe(() => {
@@ -27,12 +20,6 @@ const store = configureStore();
 //   console.log(visibleExpenses);
 // })
 
-
-//addExpense -> Water Bill
-store.dispatch(addExpense({description: 'Water Bill', amount: 4500, createdAt: 4500}));
-//addExpense -> Gas Bill
-store.dispatch(addExpense({description: 'Gas Bill', amount: 600000, createdAt: 1000}));
-store.dispatch(addExpense({description: 'Rent', amount: 109500}));
 
 //setTextFilter -> bill
 // store.dispatch(setTextFilter('bill'));
